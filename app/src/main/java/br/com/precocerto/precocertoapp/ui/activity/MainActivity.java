3e3ba1bd05609982395d80a8ebc2e44906b9b3d8
Alessandro <ale.limaalexandre@gmail.com>
2018-09-10 18:29:03 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/precocerto/precocertoapp/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.precocerto.precocertoapp.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.precocerto.precocertoapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
